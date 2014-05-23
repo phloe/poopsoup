d4ae0a2ef84f67542f095f2e68c1236838301e96
rafl <rafl@dr.dk>
2014-05-23 16:48:08 +0200

    umd'ify module

--- main.js
+++ main.js
@@ -1,33 +1,49 @@
-var key = 0;
-var topics = {};
+(function (global, define) {
+	'use strict';
 
-function publish (topic, data) {
-	var key;
-	if (topics[topic]) {
-		for (key in topics[topic]) {
-			topics[topic][key](topic, data);
+	define(function() {
+
+		var key = 0;
+		var topics = {};
+		var publishers = {};
+
+		function publish (topic, data, callback) {
+			var key;
+			if (typeof data == "function") {
+				callback = data;
+				data = null;
+			}
+			if (topics[topic]) {
+				for (key in topics[topic]) {
+					topics[topic][key](topic, data);
+				}
+			}
 		}
-	}
-}
 
-function subscribe (topic, callback) {
-	var _key = key;
-	if (!topics[topic]) {
-		topics[topic] = {};
-	}
-	if (!(_key in topics[topic])) {
-		topics[topic][_key] = callback;
-		key++;
-	}
-	return _key;
-}
+		function subscribe (topic, callback) {
+			var _key = key;
+			if (!topics[topic]) {
+				topics[topic] = {};
+			}
+			if (!(_key in topics[topic])) {
+				topics[topic][_key] = callback;
+				key++;
+			}
+			return _key;
+		}
 
-function unsubscribe (topic, key) {
-	if (topics[topic] && key in topics[topic]) {
-		delete topics[topic][key];
-	}
-}
+		function unsubscribe (topic, key) {
+			if (topics[topic] && key in topics[topic]) {
+				delete topics[topic][key];
+			}
+		}
 
-module.exports.publish = publish;
-module.exports.subscribe = subscribe;
-module.exports.unsubscribe = unsubscribe;
+		return {
+			publish: publish,
+			subscribe: subscribe,
+			unsubscribe: unsubscribe
+		};
+
+	});
+
+}(this, typeof define == 'function' && define.amd ? define : function(factory) { this.poopsoup = factory() } ));
